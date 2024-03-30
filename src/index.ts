f38f769577e411dd9fa9179cfbe36d7566b7137b
Brent de Kok <bmdekok@gmail.com>
2024-03-30 15:17:12 +0100

    Instead of setting an env variable, search for the openRCT2 folder

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@
 
 import main from './main';
 
-const { MOD_AUTHOR, MOD_NAME } = process.env;
-
 registerPlugin({
-  name: MOD_NAME!,
+  name: 'MOD_NAME',
   version: '1.0',
-  authors: [MOD_AUTHOR!],
+  authors: 'MOD_AUTHOR',
   type: 'local',
   licence: 'MIT',
   targetApiVersion: 77,
